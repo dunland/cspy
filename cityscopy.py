9af66e257e6845a35fd55212e6dde19300b228fa
Qianxun Chen <cqx931@gmail.com>
2021-11-23 17:02:29 +0100

    use seperate threshold for slider

--- cityscopy.py
+++ cityscopy.py
@@ -102,6 +102,9 @@ class Cityscopy:
         self.max_l = self.table_settings.get('max_l', 127)
         self.max_a = self.table_settings.get('max_a', 255)
         self.max_b = self.table_settings.get('max_b', 255)
+        self.slider_l = self.table_settings.get('slider_l', 127)
+        self.slider_a = self.table_settings.get('slider_a', 255)
+        self.slider_b = self.table_settings.get('slider_b', 255)
         self.quantile = self.table_settings.get('quantile', 0.5)
 
         self.sliders = [
@@ -275,6 +278,20 @@ class Cityscopy:
             # get L/a/b channels
             ch_l, ch_a, ch_b = cv2.split(lab_image)
 
+            # reduce the colors based on slider threshold
+            binary_image_slider = np.where(
+                (ch_l <= self.slider_l) & (ch_a <= self.slider_a) & (ch_b <= self.slider_b), 255, 0
+                ).astype(np.uint8)
+
+            # uncomment this to show intermediate image
+            # cv2.imshow("binary_image_slider", binary_image_slider)
+
+            # get slider values
+            mp_shared_dict['sliders'] = {
+                slider.id: slider.evaluate(binary_image_slider, video_res, block_size)
+                for slider in self.sliders
+            }
+
             # reduce the colors based on a threshold
             binary_image = np.where(
                 (ch_l <= self.max_l) & (ch_a <= self.max_a) & (ch_b <= self.max_b), 255, 0
@@ -282,12 +299,6 @@ class Cityscopy:
 
             # uncomment this to show intermediate image
             # cv2.imshow("binary_image", binary_image)
-
-            # get slider values
-            mp_shared_dict['sliders'] = {
-                slider.id: slider.evaluate(binary_image, video_res, block_size)
-                for slider in self.sliders
-            }
 
             # run through coordinates and analyse each image
             for x, y in scanner_points:
@@ -422,7 +433,7 @@ class Cityscopy:
         move_keys = ['w', 'a', 's', 'd']
         realsense_keys = ['e', 'r', 'g', 'h']
         bgr_threshold_keys = ['+', '-']
-
+        slider_threshold_keys = ['<','>']
         key = chr(cv2.waitKey(1) & 255)
 
         if key == ' ':
@@ -436,6 +447,14 @@ class Cityscopy:
             elif key == '-':
                 self.max_l -= self.magnitude
                 print("luminance threshold at ", self.max_l)
+
+        elif key in slider_threshold_keys:
+            if key == '<':
+                self.slider_l += self.magnitude
+                print("slider luminance threshold at ", self.slider_l)
+            elif key == '>':
+                self.slider_l -= self.magnitude
+                print("slider luminance threshold at ", self.slider_l)
 
         elif key in corner_keys:
             self.selected_corner = key
